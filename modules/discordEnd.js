6a318873b172337664aa14339adab8c308022a01
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-09-09 17:42:01 -0400

    better message handling

--- modules/discordEnd.js
+++ modules/discordEnd.js
@@ -1,6 +1,8 @@
 // @ts-check
 const Augur = require("augurbot-ts");
+const Discord = require("discord.js");
 const u = require("../common");
+const config = require("../config.json");
 
 const Module = new Augur.Module()
 .addCommand({
@@ -13,20 +15,38 @@ const Module = new Augur.Module()
     }
 })
 .addEvent("messageCreate", async (msg) => {
-    if (msg.author.bot || msg.author.system || msg.webhookId || !u.isLoaded()) return;
+    if (!msg.inGuild() || !u.isLoaded() || (msg.author.bot && msg.content.startsWith(config.msgPrefix))) return;
 
-    const sheetName = u.idsToSheets.get(msg.channelId);
-    if (!sheetName) return;
+    const sheetInfo = u.idsToSheets.get(msg.channelId);
+    if (!sheetInfo) return;
 
-    const worksheet = u.sheet.sheetsByTitle[sheetName];
+    const worksheet = u.sheet.sheetsByTitle[sheetInfo.sheet];
     if (!worksheet) return;
 
     // @ts-ignore
-    worksheet.addRows([{
-        Author: msg.author.displayName,
-        "Sent At": msg.createdTimestamp,
-        Content: u.replace(msg) || "Attachment"
-    }]);
+    worksheet.addRows(u.mapMessage(msg));
+
+    /** @type {string[]} */
+    let attachents = [];
+    if (msg.stickers.size > 0) attachents.push(msg.stickers.first()?.url ?? "STICKER");
+    if (msg.attachments.size > 0) attachents = attachents.concat(msg.attachments.map(a => a.url));
+
+    // @ts-expect-error
+    if (attachents.length > 0) u.sheet.sheetsByTitle[sheetInfo.sheet].addRows(attachents.map(a => ({ Message: `=IMAGE("${a}")`, MessageID: msg.id, ChannelID: msg.channel.id, Channel: msg.channel.name, PFP: u.avatar(Module, msg.author.id) })));
+})
+.addEvent("ready", async () => {
+    // @ts-ignore
+    const existingChannels = await u.sheet.sheetsByTitle.Channels.getRows();
+    const existingChannelCollection = new Discord.Collection(existingChannels.map(c => [c.ID, c]));
+
+    const mainServer = await Module.client.guilds.fetch(u.sf.mainServer);
+    const channels = mainServer.channels.cache.filter(c => c.isTextBased() && !c.isVoiceBased());
+
+    const diff = channels.subtract(existingChannelCollection);
+    if (diff.size === 0) return;
+
+    // @ts-ignore
+    u.sheet.sheetsByTitle.Channels.addRows(diff.map((ch) => ({ ID: ch.id, Channel: ch.name })));
 });
 
 module.exports = Module;
