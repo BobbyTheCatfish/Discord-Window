ad1fe737ca1d2d5e9917ac460ff728aff8b9d793
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-09-09 17:41:12 -0400

    add a bunch of commands

--- modules/sheetsEnd.js
+++ modules/sheetsEnd.js
@@ -1,6 +1,7 @@
 // @ts-check
 const Augur = require("augurbot-ts");
 const Discord = require("discord.js");
+const moment = require("moment-timezone");
 const u = require("../common");
 const config = require("../config.json");
 
@@ -17,7 +18,7 @@ const Module = new Augur.Module();
  * @returns {string | Discord.GuildTextBasedChannel}
  */
 function getChannel(sheetName, row) {
-    const id = row.ChannelID ?? u.idsToSheets.find(f => f.name === sheetName && f.name !== "Other")?.id;
+    const id = row.ChannelID ?? u.idsToSheets.find(f => f.sheet === sheetName && f.sheet !== "Other")?.id;
     if (!id) return "[No ID]";
 
     const channel = Module.client.channels.cache.get(id);
@@ -46,9 +47,81 @@ function fetch(name, rows, row) {
     return true;
 }
 
+/** @param {boolean} newState  */
+function pause(newState) {
+    u.setPaused(newState);
+    return newState ? "[Paused]" : "[Resumed]";
+}
+
+function restart() {
+    setTimeout(async () => {
+        await Module.client.destroy();
+        process.exit();
+    }, 5 * 60_000);
+
+    return '[Restarting...]';
+}
+
+function ping() {
+    return `[Pong] ${u.time()}`;
+}
+
+/** @type {Command} */
+function follow(name, rows, row) {
+    if (!row.ChannelID) return "[No ID]";
+
+    const channel = getChannel(name, row);
+    if (typeof channel === "string") return channel;
+
+    if (u.idsToSheets.has(channel.id)) return "[Already Following]";
+    u.idsToSheets.set(channel.id, { id: channel.id, name: channel.name, sheet: "Other" });
+
+    return "[Followed]";
+}
+
+/** @type {Command} */
+function unfollow(name, rows, row) {
+    if (!row.ChannelID) return "[No ID]";
+
+    const channel = getChannel(name, row);
+    if (typeof channel === "string") return channel;
+
+    if (!u.idsToSheets.has(channel.id)) return "[Not Following]";
+    u.idsToSheets.delete(channel.id);
+
+    return "[Followed]";
+}
+
+function following() {
+    return u.idsToSheets.map(s => s.name).join("\n") || "None";
+}
+
+/** @type {Command} */
+function sendMsg(name, rows, row) {
+    const channel = getChannel(name, row);
+    if (typeof channel === "string") return channel;
+
+    const content = row.MessageID;
+    if (!content) return "[No Message Content]";
+
+    channel.send(content);
+    return "[Sent]";
+}
+
+/** @type {Record<string, Command>} */
+const commands = {
+    ping,
+    restart,
+    send: sendMsg,
+    follow,
+    unfollow,
+    following,
+    fetch,
+    pause: () => pause(true),
+    resume: () => pause(false)
+};
+
 Module.setClockwork(() => {
-    // handle sheet > server
-    let updatePromises = [];
     let sendPromises = [];
     const doStuff = async (sheetName, id) => {
         // @ts-ignore
